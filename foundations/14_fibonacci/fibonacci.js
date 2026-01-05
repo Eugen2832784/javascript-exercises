9d4124d1d066a9cebdd6fadb55137edd5eac535e
EugeneDev <kovalonokjevgenij@gmail.com>
2026-01-05 10:10:02 +0200

    14 task

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let a = 0;
+  let b = 1;
 
+  for (let i = 2; i <= num; i++) {
+    let temp = a;
+    a = b;
+    b = temp + b;
+  }
+  return b;
 };
 
 // Do not edit below this line
