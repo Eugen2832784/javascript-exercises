51c444fc44054308e104cddfbd5a7f2c127d0456
EugeneDev <kovalonokjevgenij@gmail.com>
2026-01-05 09:52:15 +0200

    11 - 13 complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  text = text.toLowerCase();
+  let newArr = [];
+  let signs = ", ";
+  let arr = text.split("");
+  arr.forEach((element) => {
+    if (!signs.includes(element)) {
+      newArr.push(element);
+    }
+  });
+  let newArr1 = newArr.reverse();
+  if (newArr1 == newArr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
